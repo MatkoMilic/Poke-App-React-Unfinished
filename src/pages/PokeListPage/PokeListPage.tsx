4fa6b4d2dbab80fe9724069ea2bd429650b36bac
Matko Milic <59444128+MatkoMilic@users.noreply.github.com>
2021-12-06 17:36:28 +0100

    feat(src/pages/pokelistpage/pokelistpage.tsx): add new class name which receives style from sass

--- src/pages/PokeListPage/PokeListPage.tsx
+++ src/pages/PokeListPage/PokeListPage.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import classes from "./Styles.module.scss";
 
 const PokeListPage = (): JSX.Element => {
   return (
-    <div className="PokeListPage">
+    <div className={classes.PokeListPage}>
       <h1>List of pokemons is here</h1>
     </div>
   );
