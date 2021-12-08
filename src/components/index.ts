5e2a846e7cc7abfe384ede0706f464c58b040409
Matko Milic <59444128+MatkoMilic@users.noreply.github.com>
2021-12-08 09:50:56 +0100

    feat(pages/index.ts, components/index.ts): update indexes

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,9 @@
-export { PageContainer } from "./PageContainer/index";
-export { ThemeContext, ThemeProvider } from "./ThemeProvider/index";
+export { PageContainer } from "./PageContainer";
+export { PageContainerLogin } from "./PageContainerLogin";
+export { ThemeContext, ThemeProvider } from "./ThemeProvider";
+export { FormikForm } from "./FormikForm";
+export { LoginForm } from "./LoginForm";
+export { TextField } from "./TextField";
+export { Header } from "./Header";
+export { HeaderLogin } from "./HeaderLogin";
+export { ProtectedRoutes } from "./ProtectedRoutes";
