6ae5d11725b65c9c5fd81c9a20067d7ef33c46e9
Matko Milic <59444128+MatkoMilic@users.noreply.github.com>
2021-12-08 16:52:23 +0100

    fix(urls.ts): fix url being wrong by lacking / at the end

--- src/constants/urls/urls.ts
+++ src/constants/urls/urls.ts
@@ -1,6 +1,6 @@
 const BASE_URL = "https://pokeapi.co/api/v2";
 
 export const urls = {
-  pokemonsUrl: `${BASE_URL}/pokemon`,
+  pokemonsUrl: `${BASE_URL}/pokemon/`,
   pokemonsLimitTo20Url: `${BASE_URL}/pokemon?limit=20/`,
 };
