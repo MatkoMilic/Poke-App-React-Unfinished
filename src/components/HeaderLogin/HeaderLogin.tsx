9dcfd949ae3734b794f5bea02795c6d559a3da02
Matko Milic <59444128+MatkoMilic@users.noreply.github.com>
2021-12-08 09:42:06 +0100

    feat(components/headerlogin/): add special header for login page

---
+++ src/components/HeaderLogin/HeaderLogin.tsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from "react";
+import { BiMenuAltRight } from "react-icons/bi";
+import { AiOutlineClose } from "react-icons/ai";
+import classes from "./Styles.module.scss";
+import { Link, Outlet } from "react-router-dom";
+import { ThemeContext } from "../ThemeProvider";
+
+interface ISize {
+  width: number;
+  height: number;
+}
+
+const HeaderLogin = (): JSX.Element => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [size, setSize] = useState<ISize>();
+  const { toggleTheme } = React.useContext(ThemeContext);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (size && size.width > 768 && menuOpen) {
+      setMenuOpen(false);
+    }
+  }, [size?.width, menuOpen, size]);
+
+  const menuToggleHandler = () => {
+    setMenuOpen((p) => !p);
+  };
+
+  return (
+    <header className={classes.header}>
+      <div className={classes.header__content}>
+        <Link to="/" className={classes.header__content__logo}>
+          2FRONT
+        </Link>
+        <nav
+          className={`${classes.header__content__nav} ${
+            menuOpen && size && size.width < 768 ? classes.isMenu : ""
+          }`}
+        >
+          <ul>
+            <li>
+              <button onClick={toggleTheme}>Toggle Theme</button>
+            </li>
+            <Outlet />
+          </ul>
+        </nav>
+        <div className={classes.header__content__toggle}>
+          {!menuOpen ? (
+            <BiMenuAltRight onClick={menuToggleHandler} />
+          ) : (
+            <AiOutlineClose onClick={menuToggleHandler} />
+          )}
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default HeaderLogin;
