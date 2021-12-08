6557c813a1e3c15116424f8960372ef7dea9bfae
Matko Milic <59444128+MatkoMilic@users.noreply.github.com>
2021-12-08 09:38:18 +0100

    feat(constants/localstoragefunctions): add local storage functions and index for constants

---
+++ src/constants/index.ts
@@ -0,0 +1,7 @@
+import {
+  getActiveUser,
+  getActiveUserDetails,
+  removeActiveUser,
+} from "./localStorageFunctions/localStorageFunctions";
+
+export { getActiveUser, getActiveUserDetails, removeActiveUser };
