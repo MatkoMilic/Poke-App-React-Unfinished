ba603cc9dc1518df04a992a2ef380f04832de06e
Matko Milic <59444128+MatkoMilic@users.noreply.github.com>
2021-12-08 09:48:01 +0100

    feat(loginpage.tsx): add special page container for loginpage, add form and styles to it

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,10 +1,32 @@
 import React from "react";
+import { FormikForm, ThemeContext } from "../../components";
+import TwoFrontDark from "../../assets/TwoFrontDark.jpg";
+import TwoFrontLight from "../../assets/TwoFrontLight.png";
+import classes from "./Styles.module.scss";
+import { PageContainerLogin } from "../../components";
 
 const LoginPage = (): JSX.Element => {
+  const { isThemeDark } = React.useContext(ThemeContext);
   return (
-    <div className="LoginPage">
-      <h1>Please login in</h1>
-    </div>
+    <PageContainerLogin>
+      <div className={classes.containerMain}>
+        <div className="LoginPage">
+          <FormikForm />
+          <h1 className={classes.loginInfo}>
+            {
+              "Please login in, if you don't have an account you will automatically get one."
+            }
+          </h1>
+        </div>
+        <div className={classes.divImage}>
+          {isThemeDark ? (
+            <img className={classes.image} src={TwoFrontDark} alt="" />
+          ) : (
+            <img className={classes.image} src={TwoFrontLight} alt="" />
+          )}
+        </div>
+      </div>
+    </PageContainerLogin>
   );
 };
 
